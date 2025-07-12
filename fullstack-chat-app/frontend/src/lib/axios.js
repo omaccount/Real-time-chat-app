ec33bc5edfd9f5ede56d46efa4d595e04377d17c
Om Prakash <om482291@gmail.com>
2025-07-13 00:30:25 +0530

    update live backend site url's

--- fullstack-chat-app/frontend/src/lib/axios.js
+++ fullstack-chat-app/frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  baseURL: import.meta.env.MODE === "development" ? "https://real-time-chat-app-6orq.onrender.com": "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://real-time-chat-app-25ow.onrender.com": "/api",
   withCredentials: true,
 });
--- fullstack-chat-app/frontend/src/store/useAuthStore.js
+++ fullstack-chat-app/frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
-const BASE_URL = import.meta.env.MODE === "development" ? "https://real-time-chat-app-6orq.onrender.com" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://real-time-chat-app-25ow.onrender.com" : "/";
 
 
 export const useAuthStore = create((set, get) => ({
